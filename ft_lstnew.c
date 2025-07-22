a5118a3f9d9de8d988d4c30b93fd100be1e62657
Corentin Gordeeff <cgordeef@cbr8s6.42barcelona.com>
2025-07-22 18:04:48 +0200

    adding main into ft_lstnew.c file

--- ft_lstnew.c
+++ ft_lstnew.c
@@ -6,13 +6,36 @@
 /*   By: cgordeef <cgordeef@student.42barcelon      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/30 16:52:29 by cgordeef          #+#    #+#             */
-/*   Updated: 2025/06/30 16:53:16 by cgordeef         ###   ########.fr       */
+/*   Updated: 2025/07/22 18:04:09 by cgordeef         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stdlib.h>
 
 t_list	*ft_lstnew(void *content)
 {
+	t_list	*new_node;
 
+	new_node = malloc(sizeof(t_list));
+	if (new_node == NULL)
+	{
+		return (NULL);
+	}
+	new_node->content = content;
+	new_node->next = NULL;
+	return (new_node);
 }
+/*
+#include <stdio.h>
+int	main(void)
+{
+	t_list	*node;	
+	char	string[] = "Hello world";
+	node = ft_lstnew(string);
+	printf("content within new node:%s\n", (char *)node->content);
+	printf("content within new node:%p\n", (t_list *)node->next);
+
+	return (0);
+}
+*/
