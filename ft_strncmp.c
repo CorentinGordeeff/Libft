0fde6412aa7b71e7174acf696e0f0e36a109f7c9
Corentin <corentingordeeff@gmail.com>
2026-01-10 14:35:58 +0100

    fixed strncmp

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -17,11 +17,7 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	size_t	i;
 
 	i = 0;
-	if (n == 0)
-	{
-		return (0);
-	}
-	while (s1[i] != '\0' && s2[i] != '\0' && i < n - 1)
+	while (i < n && s1[i] != '\0' && s2[i] != '\0')
 	{
 		if (s1[i] != s2[i])
 		{
@@ -29,7 +25,7 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 		}
 		i++;
 	}
-	return (s1[i] - s2[i]);
+	return (0);
 }
 /*
 #include <stdio.h>
